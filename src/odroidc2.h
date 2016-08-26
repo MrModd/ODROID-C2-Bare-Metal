a157f8b55cc6f32ddb56474c3724afa72db9e2b9
Federico Cosentino <fed.cosentino@gmail.com>
2016-08-26 22:49:55 +0200

    Minor change

--- src/odroidc2.h
+++ src/odroidc2.h
@@ -28,7 +28,7 @@
 		(1u<<(bit))
 
 /* Set the n-th bit of a 64bit variable */
-#define BIT2MASK_64(bit)            \
+#define BIT2MASK_64(bit)         \
 		(1ull<<(bit))
 
 /* Set a bitmask in a register preserving
